723c9bfce264ac713511afc7d85033f260773c66
tnantaki <tinyhave@gmail.com>
2024-08-30 16:18:10 +0700

    Update generate certs

--- setup/generate-certs.sh
+++ setup/generate-certs.sh
@@ -10,19 +10,19 @@ openssl genrsa -out certs/root-ca-key.pem 2048
 openssl req -x509 -new -nodes -key certs/root-ca-key.pem -days 3650 -out certs/root-ca.pem -subj "/C=TH/ST=None/L=Bangkok/O=42/CN=ELK-CA"
 
 # Generate a key for Elasticsearch
-openssl genrsa -out certs/elasticsearch-key.pem 2048
+openssl genrsa -aes256 -passout pass:$ELASTIC_PASSWORD -out certs/elasticsearch-key.pem 2048
 
 # Generate a Certificate Signing Request (CSR) for Elasticsearch
-openssl req -new -key certs/elasticsearch-key.pem -out certs/elasticsearch.csr -subj "/C=TH/ST=None/L=Bangkok/O=42/CN=elasticsearch"
+openssl req -new -key certs/elasticsearch-key.pem  -passin pass:$ELASTIC_PASSWORD -out certs/elasticsearch.csr -subj "/C=TH/ST=None/L=Bangkok/O=42/CN=elasticsearch"
 
 # Generate the certificate for Elasticsearch
 openssl x509 -req -in certs/elasticsearch.csr -CA certs/root-ca.pem -CAkey certs/root-ca-key.pem -CAcreateserial -out certs/elasticsearch.pem -days 365
 
 # Generate a key for Kibana
-openssl genrsa -out certs/kibana-key.pem 2048
+openssl genrsa -aes256 -passout pass:$KIBANA_PASSWORD -out certs/kibana-key.pem 2048
 
 # Generate a Certificate Signing Request (CSR) for Kibana
-openssl req -new -key certs/kibana-key.pem -out certs/kibana.csr -subj "/C=TH/ST=None/L=Bangkok/O=42/CN=kibana"
+openssl req -new -key certs/kibana-key.pem -passin pass:$KIBANA_PASSWORD -out certs/kibana.csr -subj "/C=TH/ST=None/L=Bangkok/O=42/CN=kibana"
 
 # Generate the certificate for Kibana
 openssl x509 -req -in certs/kibana.csr -CA certs/root-ca.pem -CAkey certs/root-ca-key.pem -CAcreateserial -out certs/kibana.pem -days 365
